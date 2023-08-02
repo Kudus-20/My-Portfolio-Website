47f5a28baead7a958ce4f40e25974b0891964fca
Kudus-20 <121691255+Kudus-20@users.noreply.github.com>
2023-08-02 14:25:47 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import Typed from "typed.js";
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var typed = new Typed(".text", {
+//     strings: [
+//       "Frontend Developer",
+//       "UI/UX Designer",
+//       "Photographer/Videographer",
+//     ],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true,
+//   });
+// });
+
+var typed = Typed(".text", {
+  StringS: ["Frontend Developer", "Entrepreneur", "Web Developer"],
+  TypeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
